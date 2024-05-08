318e6f4211c21e7dbc971c30ce4f74e09dd51c2a
hdinjos <hdinjos@gmail.com>
2024-05-08 16:37:55 +0700

    change menu ant route

--- resources/views/gal.blade.php
+++ resources/views/gal.blade.php
@@ -5,17 +5,17 @@
 @section('header-content')
     <div class="row mb-2">
         <div class="col-sm-6">
-            <h1 class="m-0">Dashboard</h1>
+            <h1 class="m-0">Gallery</h1>
         </div>
         <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active">Dashboard v1</li>
+                <li class="breadcrumb-item"><a href="#">Home 1</a></li>
+                <li class="breadcrumb-item active">Dashboard v2</li>
             </ol>
         </div>
     </div>
 @endsection
 
 @section('main-content')
-    <h4>Halo dunia</h4>
+    <h4>Halo kamu</h4>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+	return view('index');
+});
+
+Route::get('/gal', function () {
+	return view('gal');
 });
